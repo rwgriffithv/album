2d663d7dfd76dd926452c7f2dee412ac1af3e703
rwgriffithv <rwgriffithv@gmail.com>
2023-09-18 20:28:41 -0700

    slight restructuring, preparing for next-js with typescript for frontend

--- python/album-db/restapi.py
+++ python/album-db/restapi.py
@@ -3,9 +3,23 @@ from flask import Flask
 _app = Flask(__name__)
 
 
+_users = {"ZC": {"id": 0, "location": "MA"}, "RG": {"id": 0, "location": "CA"}}
+
+
 @_app.route("/")
 def hello_world():
     return "This is the Album backend!!"
+
+
+@_app.route("/user")
+def user_list():
+    return [uname for uname in _users.keys()]
+
+
+@_app.route("/user/<username>")
+def user_att(username):
+    att = _users[username]
+    return att if att else {}
 
 
 if __name__ == "__main__":
